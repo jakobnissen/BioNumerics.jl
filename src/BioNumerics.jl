c24681286c2169bf2edc98eb229aceaa8dd03b49
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2021-08-20 19:23:36 +0200

    Refactor

--- src/BioNumerics.jl
+++ src/BioNumerics.jl
@@ -7,156 +7,25 @@ databases, and thus automate some of the database management.
 """
 module BioNumerics
 
-using UUIDs
 using Dates
-using ODBC
 using CSV
-using DBInterface
-using DataFrames # TODO: Is this necessary?
+#using ODBC
+#using DBInterface
+#using DataFrames # TODO: Is this necessary?
 
-const DATETIME_FORMAT = dateformat"dd/mm/yyyy HH.MM"
-const DATE_FORMAT = dateformat"dd/mm/yyyy"
-
-const COLUMNS = [
-    Symbol("(Skal ikke ændres) Prøve"),
-    Symbol("(Skal ikke ændres) Kontrolsum for række"),
-    Symbol("(Skal ikke ændres) Ændret"),
-    Symbol("Prøve id"),
-    Symbol("Internt nr."),
-    Symbol("Sags ID"),
-    :Materiale,
-    :Oprettet,
-    :Dyreart,
-    :Modtagelsestidspunkt,
-    :Udtagelsesdato
-]
 
 include("hosts.jl")
 using .Hosts
 
-struct VNumber
-    x::UInt32
-end
+include("vetlims.jl")
 
-function VNumber(s::Union{String, SubString{String}})
-    if ncodeunits(s) != 10 || codeunit(s, 1) != UInt8('V')
-        error("Invalid VNumber: \"", s, '"')
-    end
-    VNumber(parse(UInt32, view(s, 2:10)))
-end 
-
-Base.show(io::IO, v::VNumber) = print(io, 'V', lpad(v.x, 9, '0'))
-
-struct SagsNumber
-    numbers::UInt32
-    letters::UInt32
-end
-
-function SagsNumber(s::Union{String, SubString{String}})
-    if ncodeunits(s) != 16 || !startswith(s, "SAG-") || codeunit(s, 10) != UInt8('-')
-        error("Invalid SagsNumber: \"", s, '"')
-    end
-    numbers = parse(UInt32, view(s, 5:9))
-    letters = parse(UInt32, view(s, 11:16), base=36)
-    SagsNumber(numbers, letters)
-end
-
-function Base.show(io::IO, x::SagsNumber)
-    print(io, "SAG-", x.numbers, '-', uppercase(string(x.letters, base=36)))
-end
-
-# Dummy object
-struct Material end
-
-struct LIMSRow
-    id::UInt128
-    modified::DateTime
-    samplenum::NTuple{2, UInt16}
-    vnum::VNumber
-    sag::SagsNumber
-    material::Union{Nothing, Material}
-    created::DateTime
-    host::Host
-    receivedate::DateTime
-    sampledate::Union{Nothing, Date}
-end
-
-function parse_samplenum(s::AbstractFloat)
-    fraction, integer = modf(s)
-    while !iszero(modf(round(fraction, digits=8))[1])
-        fraction *= 10
-    end
-    return UInt16(integer), UInt16(fraction)
-end
-
-function parse_lims(io::IO)
-    csv = CSV.File(io)
-    if propertynames(csv) != COLUMNS
-        error("Found wrong columns, expected $COLUMNS")
-    end
-    nothing
-end
-
-macro ornothing(expr)
-    quote
-        local res = $(esc(expr))
-        ismissing(res) && return nothing
-        res
-    end
-end
-
-function tryparse_lims(row::CSV.Row)::Union{Nothing, LIMSRow}
-    id = parse_limsid(row[1])
-    # we skip the hash at col 2, we don't need to store it
-    modified = DateTime(row[3], DATETIME_FORMAT)
-    samplenum = parse_samplenum(row[4])
-    vnum = VNumber(@ornothing(row[5]))
-    sag = SagsNumber(@ornothing(row[6]))
-    material = if ismissing(row[7]) # TODO
-        nothing
-    else
-        Material()
-    end
-    created = DateTime(row[8], DATETIME_FORMAT)
-    host = if ismissing(row[9])
-        nothing
-    else
-        Hosts.unknown_duck ## TODO
-    end
-    receivedate = DateTime(row[10], DATETIME_FORMAT)
-    sampledate = if ismissing(row[11])
-        nothing
-    else
-        Date(row[11], DATE_FORMAT)
-    end
-    LIMSRow(
-        id,
-        modified,
-        samplenum,
-        vnum,
-        sag,
-        material,
-        created,
-        host,
-        receivedate,
-        sampledate
-    )
-end
-
-# It's in UUID format, but it somehow has values not permitted in proper
-# UUID. So I leverage the UUID parser, but return an UInt128
-parse_limsid(s::Union{String, SubString{String}}) = UInt128(UUID(s))
-
-
-
-
+#=
 module Viruses
 @enum Virus::UInt8 Negative InfA InfB
 export Virus
 end
 using .Viruses
 
-#=
 function establish_connection(server::AbstractString, database::AbstractString)
     Sys.isapple() && ODBC.setunixODBC()
     if !haskey(ODBC.drivers(), "ms")
---
+++ src/vetlims.jl
@@ -0,0 +1,127 @@
+const DATETIME_FORMAT = dateformat"dd/mm/yyyy HH.MM"
+const DATE_FORMAT = dateformat"dd/mm/yyyy"
+
+"Columns from CSV file exported from VetLIMS, in order"
+const COLUMNS = [
+    Symbol("(Skal ikke ændres) Prøve"),
+    Symbol("(Skal ikke ændres) Kontrolsum for række"),
+    Symbol("(Skal ikke ændres) Ændret"),
+    Symbol("Prøve id"),
+    Symbol("Internt nr."),
+    Symbol("Sags ID"),
+    :Materiale,
+    :Oprettet,
+    :Dyreart,
+    :Modtagelsestidspunkt,
+    :Udtagelsesdato
+]
+
+struct SampleNumber
+    num::UInt16
+    subnum::UInt16
+end
+
+SampleNumber(x::Integer) = SampleNumber(UInt16(x), 0)
+
+function SampleNumber(x::AbstractFloat)
+    fraction, integer = modf(x)
+    fraction = round(fraction, digits=8)
+    while !iszero(modf(fraction)[1])
+        fraction *= 10
+        fraction = round(fraction, digits=8)
+    end
+    return SampleNumber(UInt16(integer), UInt16(fraction))
+end
+
+function Base.show(io::IO, x::SampleNumber)
+    print(io, summary(x), '(', x.num, ", ", x.subnum, ')')
+end
+
+struct VNumber
+    x::UInt32
+end
+
+function VNumber(s::Union{String, SubString{String}})
+    if ncodeunits(s) != 10 || codeunit(s, 1) != UInt8('V')
+        error("Invalid VNumber: \"", s, '"')
+    end
+    VNumber(parse(UInt32, view(s, 2:10)))
+end 
+
+Base.show(io::IO, v::VNumber) = print(io, summary(v), "(\"V", string(v.x, pad=9), "\")")
+
+struct SagsNumber
+    numbers::UInt32
+    letters::UInt32
+end
+
+function SagsNumber(s::Union{String, SubString{String}})
+    if ncodeunits(s) != 16 || !startswith(s, "SAG-") || codeunit(s, 10) != UInt8('-')
+        error("Invalid SagsNumber: \"", s, '"')
+    end
+    numbers = parse(UInt32, view(s, 5:9))
+    letters = parse(UInt32, view(s, 11:16), base=36)
+    SagsNumber(numbers, letters)
+end
+
+function Base.show(io::IO, x::SagsNumber)
+    letters = uppercase(string(x.letters, base=36, pad=6))
+    print(io, summary(x), "(\"SAG-", string(x.numbers, pad=5), '-', letters, "\")")
+end
+
+# Dummy object
+struct Material end
+
+struct LIMSRow
+    samplenum::SampleNumber
+    vnum::VNumber
+    sag::SagsNumber
+    sampledate::Union{Nothing, Date}
+    host::Host
+    material::Union{Nothing, Material}
+    created::DateTime
+    receivedate::DateTime
+end
+
+function parse_lims(io::IO)
+    csv = CSV.File(io)
+    if propertynames(csv) != COLUMNS
+        error("Found wrong columns, expected $COLUMNS")
+    end
+    return map(LIMSRow, csv)
+end
+
+function LIMSRow(row::CSV.Row)
+    # We skip rows 1, 2 and 3, which are internal fields. They are, respectively:
+    # 128-bit UUID, checksum, datetime modified
+    samplenum = SampleNumber(row[4])
+    vnum = VNumber(row[5])
+    sag = SagsNumber(row[6])
+    material = if ismissing(row[7]) # TODO
+        nothing
+    else
+        Material()
+    end
+    created = DateTime(row[8], DATETIME_FORMAT)
+    host = if ismissing(row[9])
+        nothing
+    else
+        Hosts.unknown_duck ## TODO
+    end
+    receivedate = DateTime(row[10], DATETIME_FORMAT)
+    sampledate = if ismissing(row[11])
+        nothing
+    else
+        Date(row[11], DATE_FORMAT)
+    end
+    LIMSRow(
+        samplenum,
+        vnum,
+        sag,
+        sampledate,
+        host,
+        material,
+        created,
+        receivedate,
+    )
+end
